3657ec568136e09014fcf143841898ac18359dc0
noodlexpoodle <jaydtran@hotmail.com>
2018-10-13 17:42:10 +0800

    commit

---
+++ Filter.py
@@ -0,0 +1,29 @@
+gpa = []
+i = 1
+while i == 1:
+    var = input('Enter your grade. Type "Done" to finish ')
+    if var.lower() == 'done':
+        i = 0
+    else:
+        gpa.append(var)
+print(gpa)
+
+#by filter
+def remove_bad_grade (grade):
+    return grade !='F'
+print(list(filter(remove_bad_grade,gpa)))
+
+#by loop
+no_fail = []
+for grade in gpa:
+    if grade != 'F':
+        no_fail.append(grade)
+print(no_fail)
+
+#by comprehension
+no_fail = [grade for grade in gpa if grade != 'F']
+print(no_fail)
+
+#by map
+no_fail = list(map(remove_bad_grade,gpa))
+print(no_fail)
--- Maps.py
+++ Maps.py
@@ -5,8 +5,15 @@ def jumble(word):
     anagram = list(word)
     shuffle(anagram) #shuffle the list
     return ''.join(anagram)
-
-words = ['apple','pear','melon']
+#words = ['apple','pear','melon']
+words = []
+i = 1
+while i == 1:
+    word = input('Enter word. Type "Done" to stop ')
+    if word.lower() != 'done':
+        words.append(word)
+    else:
+        i = 0
 #anagram is the orig list, anagramS is the new empty list for the other list to transform into
 anagrams = []
 
