29437a98c9621415dd9f152f3f08fa855780d8d0
noodlexpoodle <jaydtran@hotmail.com>
2019-11-10 18:04:47 +0800

    update

--- Calc.py
+++ Calc.py
@@ -3,9 +3,15 @@
 # print (name,'is',age,'year old')
 
 #Calc area
-def are(radius):
-    radius = int(input  ('Input radius: '))
-    area = radius**2*3.142
-    print ('The area is: ',area)
+# def are(radius):
+#     radius = int(input  ('Input radius: '))
+#     area = radius**2*3.142
+#     print ('The area is: ',area)
+#
+# are(radius='1')
 
-are(radius='1')
+# def are(radius):
+#     pass
+radius = int(input('Input radius: '))
+area = radius**2*3.142
+print ('Area is ',area)
