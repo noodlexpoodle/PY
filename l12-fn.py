4b3655491bb93e27368d0fe64db21633d7bad938
noodlexpoodle <jaydtran@hotmail.com>
2019-11-16 20:40:11 +0800

    ok

--- l12-fn.py
+++ l12-fn.py
@@ -5,8 +5,27 @@
 # greet()
 # greet()
 # greet()
-def greet(a):
-    print('hello '+a)
+def greet(a='sir',d='day'):
+    print(f'Good {d}, {a}')
+# f'text {variable}
+# f is for formatting, the {} will be the variable
+b=input("What's your name ")
+c=input("What's the time ")
+if b is not None and c is not None:
+    greet(b,c)
+    print(1)
+elif b is not None and c is None:
+    greet(c='day')
+    print(2)
+elif b is None:
+    greet(b='sir')
+    print(3)
+else:
+    greet()
+    print(4)
+    pass
 
-b=input('Name?')
-greet(b)
+# First define the function with def name (var)
+# Then call function by name and input the variable
+# SIMPLE!
+# You can input a default value in case value is missing
