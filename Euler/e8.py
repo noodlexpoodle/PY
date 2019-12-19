2e1e5624c7e4df36f7566ed38ff1f01e09245c9f
noodlexpoodle <jaydtran@hotmail.com>
2019-12-19 14:08:24 +0800

    ok

--- Euler/e8.py
+++ Euler/e8.py
@@ -2,4 +2,8 @@
 
 number = int(input('Input number'))
 number1 = [int(x) for x in number]
-for d in number:
+for d in number1:
+    if d == 0:
+        number1.remove(d)
+
+print(f'The list is {number1}')
