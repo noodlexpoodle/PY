46d019a7c026c22fbdc1458ee3a36301abf8c662
noodlexpoodle <jaydtran@hotmail.com>
2019-12-21 00:44:16 +0800

    ok

--- Euler/e8.py
+++ Euler/e8.py
@@ -1,9 +1,37 @@
 #find largest product
+# let's look at 3 digits
+number = str(123045426340545254056564650565)
+# number1 = [int(x) for x in number]
+numa = list(number)
+print (numa)
+max = int()
+cur = int()
+n = 0
+print (len(numa))
+while n < (len(numa)-13):
+    n1 = int(numa[n])
+    n2 = int(numa[n+1])
+    n3 = int(numa[n+2])
+    n4 = int(numa[n+3])
+    n5 = int(numa[n+4])
+    n6 = int(numa[n+5])
+    n7 = int(numa[n+6])
+    n8 = int(numa[n+7])
+    n9 = int(numa[n+8])
+    n10 = int(numa[n+9])
+    n11 = int(numa[n+10])
+    n12 = int(numa[n+11])
+    n13 = int(numa[n+12])
+    if n1 < n13
+    if num2 == 0:
+        n += 2
+    else:
+        cur = num1 * num2 * num3
+        if cur == 0:
+            n +=
+        n += 1
+    print (cur)
 
-number = (input('Input number'))
-number1 = [int(x) for x in number]
-for d in number1:
-    if d == 0:
-        number1.remove(d)
 
-print(f'The list is {number1}')
+
+print(f'The list is {numa}')
